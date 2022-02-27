22a9d966cd8c5b185ed80056e4bc54ef6f5aad68
boukew99 <boukewestra0@gmail.com>
2022-02-27 22:30:15 +0100

    first

---
+++ addons/scratch_canvas/canvas.gd
@@ -0,0 +1,36 @@
+extends TextureButton 
+
+var line = preload("line.tscn")
+var current_line
+
+func _on_Canvas_toggled(button_pressed):
+	if button_pressed:
+		current_line = line.instance()
+		add_child(current_line)
+		$CaptureTimer.stop()
+	else:
+		$CaptureTimer.start()
+
+
+func _on_Canvas_gui_input(event):
+	if event is InputEventMouseMotion and event.relative and pressed:
+		current_line.add_point(event.position)
+
+	
+# replace line nodes with capture for less drawing cost (visible in Remote Scene Tree)
+func _on_CaptureTimer_timeout():
+	get_viewport().set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
+	yield(VisualServer, "frame_post_draw")
+
+	var image = get_viewport().get_texture().get_data()
+
+	image.flip_y()
+	image = image.get_rect(get_global_rect())
+	
+	var tex = ImageTexture.new()
+	tex.create_from_image(image)
+	texture_normal = tex
+
+	get_tree().call_group("line", "queue_free")
+
+
