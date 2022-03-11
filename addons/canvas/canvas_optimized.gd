7929277513dbca1768e34cbd7c2e9366cfb4ea92
boukew99 <boukewestra0@gmail.com>
2022-03-11 17:22:21 +0100

    Timer -> line counter = more flexible

--- addons/canvas/canvas_optimized.gd
+++ addons/canvas/canvas_optimized.gd
@@ -1,5 +1,6 @@
 extends TextureButton
 
+var max_lines = 10 # then lines2capture 
 var line = preload("line.tscn")
 var current_line
 
@@ -7,18 +8,17 @@ func _on_Canvas_toggled(button_pressed):
 	if button_pressed:
 		current_line = line.instance()
 		add_child(current_line)
-		$CaptureTimer.stop()
 	else:
-		$CaptureTimer.start()
-
+		if get_child_count() > max_lines:
+			lines2capture()
 
 func _on_Canvas_gui_input(event):
 	if event is InputEventMouseMotion and event.relative and pressed:
 		current_line.add_point(event.position)
 
 
-# replace line nodes with capture for less drawing cost (visible in Remote Scene Tree)
-func _on_CaptureTimer_timeout():
+# capture has less drawing cost 
+func lines2capture():
 	get_viewport().set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
 	yield(VisualServer, "frame_post_draw")
 
@@ -31,8 +31,8 @@ func _on_CaptureTimer_timeout():
 	tex.create_from_image(image)
 	texture_normal = tex
 
-	# free Line2D nodes
-	for index in range(1, get_child_count()):
+	# free Line2D nodes (visible in Remote Scene Tree)
+	for index in range(get_child_count()):
 		get_child(index).queue_free()
 
 
